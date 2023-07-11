97019937232dfd18624a60298cf621851a29cba3
Alexander <alexgomezgomez15@gmail.com>
2023-07-11 12:50:29 -0400

    created person class

--- script.js
+++ script.js
@@ -18,3 +18,55 @@ class Hamster {
         return this.price;
     }
 }
+
+//Person Class
+class Person {
+    constructor(name){
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getAge(){
+        return this.age;
+    }
+
+    getWeight(){
+        return this.weight;
+    }
+
+    greet(){
+        console.log(`Hello ${this.name}`);
+    }
+
+    eat(){
+        this.weight++;
+        this.mood++;
+    }
+
+    exercise(){
+        this.weight--;
+    }
+
+    ageUP(){
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount += 10;
+    }
+
+    buyHamster(hamster){
+        this.hamsters.push(hamster);
+        mood++;
+        this.bankAccount -= hamster.getPrice();
+    }
+}
